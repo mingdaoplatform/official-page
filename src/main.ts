bd2e53503b40b38b6ab8b18ede1afce59b2eecc9
banahaker <pn0818x@outlook.com>
2022-06-17 16:42:43 +0800

    feat: global font style

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./scss/global.scss";
+import router from "./router";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");

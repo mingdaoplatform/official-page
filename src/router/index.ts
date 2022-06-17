ed2bbf6fd144b05614ff10e189328cab858c4e2f
banahaker <pn0818x@outlook.com>
2022-06-17 16:39:14 +0800

    feat: router

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/home.vue"),
+  },
+];
+
+export default createRouter({
+  routes,
+  history: createWebHistory(),
+});
